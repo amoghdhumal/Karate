ae72914c1ddc7b5e5884328f13d1ed20f73505ed
AmoghDhumal <amoghdhumal@gmail.com>
2024-04-05 05:07:13 -0700

    test

--- src/test/java/features/TestRunner.java
+++ src/test/java/features/TestRunner.java
@@ -1,11 +1,22 @@
 package features;
 
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
 import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.junit5.Karate.Test;
 
 public class TestRunner {
-	@Karate.Test
-	Karate testSample() {
-		return Karate.run("configread").relativeTo(getClass());
+	/*
+	 * @Karate.Test Karate testSample() { return
+	 * Karate.run("doDelete").relativeTo(getClass());
+	 * 
+	 * 
+	 * }
+	 */
+	@Test
+	void testSmaple() {
+		Results rs = Runner.path("classpath:feature/readcsv").parallel(2);
+		
 	}
 }
 //ce31e2e25b3ca84cb1eeba879e99a713487c21eeb322847b2135a84db185cda4
---
+++ src/test/java/util/TestKarate.java
@@ -0,0 +1,10 @@
+package util;
+
+public class TestKarate {
+	private  String callFromKarate(String name) {
+		return "Hello" + name;
+		// TODO Auto-generated method stub
+
+	}
+
+}
